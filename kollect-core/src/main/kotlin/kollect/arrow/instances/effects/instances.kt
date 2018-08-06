e2040c95e9d73aee8da354015708c21d8841b8d1
Fede Fernandez <720923+fedefernandez@users.noreply.github.com>
2018-08-06 11:18:52 +0200

    WIP

--- kollect-core/src/main/kotlin/kollect/arrow/instances/effects/instances.kt
+++ kollect-core/src/main/kotlin/kollect/arrow/instances/effects/instances.kt
@@ -8,7 +8,15 @@ import kollect.Query
 object IOKollectMonadErrorInstance : KollectMonadError<ForIO> {
     override fun ME(): MonadError<ForIO, Throwable> = IO.monadError()
 
-    override fun <A> runQuery(q: Query<A>): Kind<ForIO, A> = TODO()
+    override fun <A> runQuery(q: Query<A>): Kind<ForIO, A> =
+        when(q) {
+            is kollect.Sync -> IO.eval(q.action)
+            is kollect.Async -> IO.async<A> { callback ->
+                TODO()
+            }
+            is kollect.Ap<*, *> ->
+                IO.applicative().product(runQuery(q.fa), runQuery(q.ff)).fix().map { tuple2 -> tuple2.b(tuple2.a) }.hk
+        }
 }
 
 object IOKollectMonadErrorInstanceImplicits {
