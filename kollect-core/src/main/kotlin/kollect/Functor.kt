e3f8436d0c104705ae815be07bf7ce4f6a065476
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-26 20:41:21 +0100

    remove some Functor non needed syntax

--- kollect-core/src/main/kotlin/kollect/Functor.kt
+++ kollect-core/src/main/kotlin/kollect/Functor.kt
@@ -3,6 +3,4 @@ package kollect
 import arrow.Kind
 import arrow.typeclasses.Functor
 
-fun <F, A> Kind<F, A>.void(FF: Functor<F>): Kind<F, Unit> = this.`as`(FF, Unit)
-
-fun <F, A, B> Kind<F, A>.`as`(FF: Functor<F>, b: B): Kind<F, B> = FF.run { map { b } }
+fun <F, A> Kind<F, A>.void(FF: Functor<F>): Kind<F, Unit> = FF.run { this@void.`as`(Unit) }
