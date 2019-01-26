76792e392b82baf90e2f970894c1f8c7ffab9e02
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-26 17:10:24 +0100

    adapt Env to match Fetch implementation

--- kollect-core/src/main/kotlin/kollect/Env.kt
+++ kollect-core/src/main/kotlin/kollect/Env.kt
@@ -1,8 +1,8 @@
 package kollect
 
 /**
- * An environment that is passed along during the fetch rounds. It holds the cache and the list of rounds that have been
- * executed.
+ * An environment that is passed along during the kollect rounds. It holds the cache and the list of rounds that have
+ * been executed.
  */
 interface Env {
     val rounds: List<Round>
@@ -10,22 +10,24 @@ interface Env {
 }
 
 /**
- * A data structure that holds information about a request inside a fetch round.
+ * A data structure that holds information about a request inside a kollect round.
  */
-data class Request(val request: KollectRequest, val start: Long, val end: Long) {
-    val duration: Long = end - start
-}
+data class Request(
+  val request: KollectRequest,
+  val start: Long,
+  val end: Long,
+  val duration: Long = end - start
+)
 
 /**
- * A data structure that holds information about a fetch round.
+ * A data structure that holds information about a kollect round.
  */
 data class Round(val queries: List<Request>)
 
 /**
- * A concrete implementation of `Env` used in the default Fetch interpreter.
+ * A concrete implementation of `Env` used in the default Kollect interpreter.
  */
 data class KollectEnv(override val rounds: List<Round> = listOf()) : Env {
 
     override fun evolve(newRound: Round): KollectEnv = copy(rounds = rounds + newRound)
 }
-
