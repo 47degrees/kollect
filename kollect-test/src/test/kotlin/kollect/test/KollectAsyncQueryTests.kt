866bcd10d7f04285e1c9c3cd6ebe1b119d16d4ce
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-27 12:20:48 +0100

    add a couple of tests

--- kollect-test/src/test/kotlin/kollect/test/KollectAsyncQueryTests.kt
+++ kollect-test/src/test/kotlin/kollect/test/KollectAsyncQueryTests.kt
@@ -3,44 +3,59 @@ package kollect.test
 import arrow.Kind
 import arrow.core.Option
 import arrow.core.Right
+import arrow.core.Tuple2
 import arrow.effects.ForIO
 import arrow.effects.IO
-import arrow.effects.extensions.io.applicative.map
 import arrow.effects.extensions.io.concurrent.concurrent
+import arrow.effects.fix
 import arrow.effects.typeclasses.Concurrent
 import io.kotlintest.runner.junit4.KotlinTestRunner
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.AbstractStringSpec
 import kollect.DataSource
 import kollect.Kollect
+import kollect.extensions.io.timer.timer
+import kollect.extensions.kollect.monad.flatMap
+import kollect.extensions.kollect.monad.map
+import kollect.test.DataSources.Article
+import kollect.test.DataSources.Author
 import kollect.test.DataSources.article
+import kollect.test.DataSources.author
 import org.junit.runner.RunWith
+import kotlin.coroutines.EmptyCoroutineContext
 
 @RunWith(KotlinTestRunner::class)
 class KollectAsyncQueryTests : AbstractStringSpec() {
     init {
 
         "We can interpret an async fetch into an IO" {
-            fun <F> kollect(): Kollect<ForIO, DataSources.Article> = article(IO.concurrent(), 1)
+            fun kollect(): Kollect<ForIO, Article> = article(IO.concurrent(), 1)
 
-            val io = Kollect.run<ForIO>().invoke(IO.concurrent(), IO.timer(), kollect<ForIO>())
+            val io = Kollect.run<ForIO>()(IO.concurrent(), IO.timer(EmptyCoroutineContext), kollect())
+            val res = io.fix().unsafeRunSync()
+            res shouldBe Article(1, "An article with id 1")
+        }
 
-            io.map { it shouldBe DataSources.Article(1, "An article with id 1") }.unsafeToFuture
+        "We can combine several async data sources and interpret a fetch into an IO" {
+            fun <F> kollect(CF: Concurrent<F>): Kollect<F, Tuple2<Article, Author>> =
+                    article(CF, 1).flatMap<F, String, Article, Tuple2<Article, Author>>(CF) { art ->
+                        author(CF, art).map<F, String, Author, Tuple2<Article, Author>>(CF) { author ->
+                            Tuple2(art, author)
+                        }
+                    }
+
+//            CF.binding {
+//                val art = article(CF, 1).bind()
+//                val author = author(CF, art).bind()
+//                Tuple2(art, author)
+//            }
+
+            val io = Kollect.run<ForIO>()(IO.concurrent(), IO.timer(EmptyCoroutineContext), kollect(IO.concurrent()))
+            val res = io.fix().unsafeRunSync()
+
+            res shouldBe Tuple2(Article(1, "An article with id 1"), Author(2, "@egg2"))
         }
     }
-//
-//
-//
-//    "We can combine several async data sources and interpret a fetch into an IO" in {
-//        def fetch[F[_] : ConcurrentEffect]: Fetch[F, (Article, Author)] = for {
-//        art    <- article(1)
-//        author <- author(art)
-//    } yield (art, author)
-//
-//        val io = Fetch.run[IO](fetch)
-//
-//        io.map(_ shouldEqual (Article(1, "An article with id 1"), Author(2, "@egg2"))).unsafeToFuture
-//    }
 //
 //    "We can use combinators in a for comprehension and interpret a fetch from async sources into an IO" in {
 //        def fetch[F[_] : ConcurrentEffect]: Fetch[F, List[Article]] = for {
