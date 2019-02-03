b61cebc77087edc03c2bf4d942620c1bf9d225d7
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-02-03 11:44:00 +0100

    add syntax tests

--- kollect-core/src/main/kotlin/kollect/syntax.kt
+++ kollect-core/src/main/kotlin/kollect/syntax.kt
@@ -2,19 +2,6 @@ package kollect
 
 import arrow.effects.typeclasses.Concurrent
 
-object syntax {
+infix fun <F, A> A.fetch(CF: Concurrent<F>): Kollect<F, A> = Kollect.just(CF, this)
 
-    /** syntax to lift any value to the context of Kollect via just */
-    class FetchIdSyntax<A>(val a: A) : Any() {
-
-        fun <F> fetch(CF: Concurrent<F>): Kollect<F, A> =
-                Kollect.just(CF, a)
-    }
-
-    /** syntax to lift exception to Kollect errors */
-    class FetchExceptionSyntax<B>(val a: Throwable) : Any() {
-
-        fun <F> fetch(CF: Concurrent<F>): Kollect<F, B> =
-                Kollect.error(CF, a)
-    }
-}
+infix fun <F, B> Throwable.fetch(CF: Concurrent<F>): Kollect<F, B> = Kollect.error(CF, this)
