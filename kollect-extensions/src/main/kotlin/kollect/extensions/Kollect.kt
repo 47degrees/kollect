cd2cf7c0231237ea402e3af0b43ea9e100f2f12b
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-02-03 17:12:04 +0100

    use more concise alias for partially applied kollect instances

--- kollect-extensions/src/main/kotlin/kollect/extensions/Kollect.kt
+++ kollect-extensions/src/main/kotlin/kollect/extensions/Kollect.kt
@@ -1,36 +1,32 @@
 package kollect.extensions
 
-import arrow.Kind
 import arrow.core.Either
 import arrow.core.Tuple2
 import arrow.extension
 import arrow.typeclasses.Applicative
 import arrow.typeclasses.Functor
 import arrow.typeclasses.Monad
-import kollect.Kollect
-import kollect.KollectPartialOf
-import kollect.KollectResult
-import kollect.fix
+import kollect.*
 
 @extension
-interface KollectFunctor<F, I> : Functor<KollectPartialOf<F>> {
+interface KollectFunctor<F> : Functor<KollectPartialOf<F>> {
 
     fun MF(): Monad<F>
 
-    override fun <A, B> Kind<KollectPartialOf<F>, A>.map(f: (A) -> B): Kollect<F, B> =
+    override fun <A, B> KollectOf<F, A>.map(f: (A) -> B): Kollect<F, B> =
             fix().map(MF(), f)
 }
 
 @extension
-interface KollectApplicative<F, I> : Applicative<KollectPartialOf<F>>, KollectFunctor<F, I> {
+interface KollectApplicative<F, I> : Applicative<KollectPartialOf<F>>, KollectFunctor<F> {
     override fun MF(): Monad<F>
 
     override fun <A> just(a: A): Kollect<F, A> = Kollect.Unkollect(MF().just(KollectResult.Done(a)))
 
-    override fun <A, B> Kind<KollectPartialOf<F>, A>.product(fb: Kind<KollectPartialOf<F>, B>): Kollect<F, Tuple2<A, B>> =
+    override fun <A, B> KollectOf<F, A>.product(fb: KollectOf<F, B>): Kollect<F, Tuple2<A, B>> =
             fix().product<I, B>(MF(), fb)
 
-    override fun <A, B> Kind<KollectPartialOf<F>, A>.map(f: (A) -> B): Kollect<F, B> = fix().map(MF(), f)
+    override fun <A, B> KollectOf<F, A>.map(f: (A) -> B): Kollect<F, B> = fix().map(MF(), f)
 }
 
 @extension
@@ -38,12 +34,12 @@ interface KollectMonad<F, I> : Monad<KollectPartialOf<F>>, KollectApplicative<F,
 
     override fun MF(): Monad<F>
 
-    override fun <A, B> tailRecM(a: A, f: (A) -> Kind<KollectPartialOf<F>, Either<A, B>>): Kollect<F, B> =
+    override fun <A, B> tailRecM(a: A, f: (A) -> KollectOf<F, Either<A, B>>): Kollect<F, B> =
             Kollect.tailRecM(MF(), a, f)
 
-    override fun <A, B> Kind<KollectPartialOf<F>, A>.map(f: (A) -> B): Kollect<F, B> = fix().map(MF(), f)
-    
-    override fun <A, B> Kind<KollectPartialOf<F>, A>.flatMap(f: (A) -> Kind<KollectPartialOf<F>, B>): Kollect<F, B> = MF().run {
+    override fun <A, B> KollectOf<F, A>.map(f: (A) -> B): Kollect<F, B> = fix().map(MF(), f)
+
+    override fun <A, B> KollectOf<F, A>.flatMap(f: (A) -> KollectOf<F, B>): Kollect<F, B> = MF().run {
         fix().flatMap(this, f)
     }
 }
