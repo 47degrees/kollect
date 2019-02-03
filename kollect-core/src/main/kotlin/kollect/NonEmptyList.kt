cf205a0e962b24dd2162c9116eecd6053002171b
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-02-03 16:41:42 +0100

    remove scala docs

--- kollect-core/src/main/kotlin/kollect/NonEmptyList.kt
+++ kollect-core/src/main/kotlin/kollect/NonEmptyList.kt
@@ -9,17 +9,6 @@ import arrow.data.fix
 /**
  * Builds a new [List] by applying a partial function to all the elements from this [NonEmptyList] on which the function
  * is defined.
- *
- * {{{
- * scala> val nel = NonEmptyList.of(1, 2, 3, 4, 5)
- * scala> nel.collect { case v if v < 3 => v }
- * res0: scala.collection.immutable.List[Int] = List(1, 2)
- * scala> nel.collect {
- *      |  case v if v % 2 == 0 => "even"
- *      |  case _ => "odd"
- *      | }
- * res1: scala.collection.immutable.List[String] = List(odd, even, odd, even, odd)
- * }}}
  */
 fun <A, B> Kind<ForNonEmptyList, A>.collect(pf: PartialFunction<A, B>): List<B> =
         if (pf.isDefinedAt(fix().head)) {
